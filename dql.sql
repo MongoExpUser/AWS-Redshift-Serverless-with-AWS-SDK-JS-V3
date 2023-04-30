883ae973cb2427033336c98a1cfd46ee725267ea
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-04-30 01:54:41 -0500

    Update dql.sql

--- dql.sql
+++ dql.sql
@@ -1,12 +1,10 @@
-
 -- DQL
 
--- 1.  All streamed music videos ordered by producer
+-- 1.  All streamed music videos ordered by producers
 SELECT  pr.producer, ar.year, ar.title, ar.singer, ar.age
 FROM music_video.artist ar
 INNER JOIN music_video.producer pr ON  ar.title = pr.title
 ORDER BY pr.producer ASC;
-
 
 -- 2. top streams music videos
 SELECT ar.youtube_streams, ar.singer, ar.title, ar.age, pr.producer 
@@ -14,13 +12,11 @@ FROM music_video.artist ar
 INNER JOIN music_video.producer pr ON ar.title = pr.title
 ORDER BY ar.youtube_streams DESC;
 
-
--- 3. oldest-to-youngest music videos singer
+-- 3. oldest-to-yongest music videos singer
 SELECT ar.age, ar.singer
 FROM music_video.artist ar
 INNER JOIN music_video.producer pr ON ar.title = pr.title
 ORDER BY ar.age DESC;
-
 
 -- 4. youngest-to-oldest music videos singer
 SELECT ar.age, ar.singer
@@ -28,11 +24,9 @@ FROM music_video.artist ar
 INNER JOIN music_video.producer pr ON ar.title = pr.title
 ORDER BY ar.age ASC;
 
-
 -- 5. top streams music video since 2021
 SELECT ar.youtube_streams, ar.year, ar.singer, ar.title, pr.producer
 FROM music_video.artist ar
 INNER JOIN music_video.producer pr ON ar.title = pr.title
 WHERE ar.year >= 2021
 ORDER BY pr.producer ASC;
-
