ad53ccd7f01b043ccd6d65046b82d910bfcf58a2
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-04-30 00:25:17 -0500

    Create ddl.sql

---
+++ ddl.sql
@@ -0,0 +1,26 @@
+CREATE USER userone PASSWORD 'md5myPassword' CREATEUSER CREATEDB SYSLOG ACCESS UNRESTRICTED;
+
+CREATE SCHEMA IF NOT EXISTS music_video AUTHORIZATION userone;
+
+DROP TABLE IF EXISTS music_video.artist;
+CREATE TABLE IF NOT EXISTS music_video.artist (
+  id INTEGER IDENTITY(1,1),
+  year SMALLINT NOT NULL,
+  title VARCHAR(30)  NOT NULL,
+  singer VARCHAR(30) NOT NULL,
+  age SMALLINT NOT NULL,
+  youtube_streams BIGINT NOT NULL
+) COMPOUND SORTKEY (id, year);
+-- automatic table optimization using DISTKEY
+-- ALTER TABLE music.playlist ALTER DISTSTYLE AUTO; -- the default
+
+
+DROP TABLE IF EXISTS music_video.producer;
+CREATE TABLE IF NOT EXISTS music_video.producer (
+  id INTEGER IDENTITY(1,1),
+  title VARCHAR(30) NOT NULL,
+  producer VARCHAR(30) NOT NULL,
+  experience_in_years SMALLINT NOT NULL
+) COMPOUND SORTKEY (id, producer);
+-- automatic table optimization using DISTKEY
+-- ALTER TABLE music.producer ALTER DISTSTYLE AUTO; -- the default
